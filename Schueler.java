b07ed7c78609fcf0a9e9232dc1196852cc5b41be
HGSDavid <125360594+HGSDavid@users.noreply.github.com>
2023-09-13 09:48:41 +0000

    added

---
+++ Schueler.java
@@ -0,0 +1,16 @@
+class Schueler{
+
+    int groesse;
+    String name;
+    
+    //Konstruktor
+    public Schueler(){
+        groesse = 175;
+        name = "Mustermann";
+    }
+
+    //Getter
+    public String getName(){
+        return name;
+    }
+}
