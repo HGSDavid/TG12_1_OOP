2173f32c2d743ce159c96eb1a99193bab34d784d
HGSDavid <125360594+HGSDavid@users.noreply.github.com>
2023-10-26 08:32:25 +0000

    klasse Person fertig

---
+++ C00_Vererbung/Person.java
@@ -0,0 +1,27 @@
+package C00_Vererbung;
+
+/**
+ * Person
+ */
+public class Person {
+
+    private String name;
+    private String vorname;                         //erstellt mit alt + shift + pfeilnachunten
+    protected int id;
+
+    public Person(String name, String vorname) {    //erstellt mit str + . (generate Constructors)
+        this.name = name;
+        this.vorname = vorname;
+    }
+
+    public String getName() {                       //erstellt mit str + . (generate Getter)
+        return name;
+    }
+
+    @Override                                       //erstellt mit str + . (generate toString)
+    public String toString() {
+        return "Person [name=" + name + ", vorname=" + vorname + ", id=" + id + "]";
+    }
+
+    
+}
