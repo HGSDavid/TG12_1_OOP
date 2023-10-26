5e70c934567c4de3d1f351ddfef10d53a2d0d7a1
HGSDavid <125360594+HGSDavid@users.noreply.github.com>
2023-10-26 08:48:31 +0000

    startklasse fertig

--- C00_Vererbung/Startklasse.java
+++ C00_Vererbung/Startklasse.java
@@ -1,5 +1,14 @@
 package C00_Vererbung;
 
 public class Startklasse {
-    
+
+    public static void main(String[] args) {
+
+        Schueler s1 = new Schueler("Meier","Max");
+        Schueler s2 = new Schueler("Hammer","Niko","nh@web.de");
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
 }
