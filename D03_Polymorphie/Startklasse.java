0667edeecee8f297099bbb864a0554ef7672e60e
HGSDavid <125360594+HGSDavid@users.noreply.github.com>
2023-11-15 11:49:48 +0000

    scanner

--- D03_Polymorphie/Startklasse.java
+++ D03_Polymorphie/Startklasse.java
@@ -1,12 +1,17 @@
 package D03_Polymorphie;
+import java.util.Scanner;
 
 public class Startklasse {
     
     public static void main(String[] args) {
-        Mitarbeiter m1 = new Mitarbeiter(40, 12);
-        Abteilungsleiter a1 = new Abteilungsleiter(40, 120);
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("Gehalt Mitarbeiter: \t\t" + m1.berechneGehalt());
+        System.out.println("Wie viele Stunden arbeitet der Mitarbeiter?");
+        Mitarbeiter m1 = new Mitarbeiter(s.nextInt(), 12);
+        System.out.println("Wie viele Stunden arbeitet der Abteilungsleiter?");
+        Abteilungsleiter a1 = new Abteilungsleiter(s.nextInt(), 120);
+
+        System.out.println("\nGehalt Mitarbeiter: \t\t" + m1.berechneGehalt());
         System.out.println("Gehalt Abteilungsleiter: \t" + a1.berechneGehalt());
     }
 }
