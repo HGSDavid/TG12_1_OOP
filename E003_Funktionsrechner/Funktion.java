6d1420b579f36e91d1c3e95f7749c1023f7b170c
David <125360594+HGSDavid@users.noreply.github.com>
2024-01-10 12:51:40 +0100

    Update Funktion.java

--- E003_Funktionsrechner/Funktion.java
+++ E003_Funktionsrechner/Funktion.java
@@ -1,5 +1,27 @@
 package E003_Funktionsrechner;
 
-public class Funktion {
+public abstract class Funktion {
     
+    protected double a;
+    protected double b;
+    protected double c;
+    protected double xMin;
+    protected double xMax;
+
+    public abstract double berechneFunktionswert();
+    
+    public double berechneFlache(int n){
+        return -1;
+    }
+
+    public void setzeParameter (double pa, double pb, double pc){
+        a=pa;
+        b=pb;
+        c=pc;
+    }
+
+    public void setzeGrenzen (double min, double max){
+        xMin = min;
+        xMax = max;
+    }
 }
