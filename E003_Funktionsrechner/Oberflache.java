04c64f05fd1a2ff8be3bc860f293c649d0e26fc0
David <125360594+HGSDavid@users.noreply.github.com>
2024-01-10 12:52:47 +0100

    Update Oberflache.java

--- E003_Funktionsrechner/Oberflache.java
+++ E003_Funktionsrechner/Oberflache.java
@@ -6,5 +6,36 @@ public class Oberflache {
     public Oberflache(Steuerung diSteuerung) {
         this.dieSteuerung = diSteuerung;
     }
-    
+
+    public void zeigeErgebnis(double flache){
+
+    }
+
+    public int gibFunktionsTyp(){
+        return -1;
+    }
+
+    public double gibXMin(){
+        return -1;
+    }
+    public double gibXMax(){
+        return -1;
+    }
+    public double gibParA(){
+        return -1;
+    }
+    public double gibParB(){
+        return -1;
+    }
+    public double gibParC(){
+        return -1;
+    }
+
+    public void clickBerechne(){
+
+    }
+
+    public void clickBeende(){
+        
+    }
 }
